ba27240108c4dbc195b72a2cd448834c0de4ba3d
KaJuan Johnson <kdjohnson@users.noreply.github.com>
2022-12-06 22:15:48 -0500

    remove import use set instead

--- 2022/d6/main.py
+++ 2022/d6/main.py
@@ -1,27 +1,19 @@
-from collections import Counter
-
-
 def part_1(s: str) -> int:
     """Solve part 1 for day 6 2022."""
-    for i in range(len(s)):
-        for j in range(i + 4, len(s)):
-            occurences = Counter(s[i:j])
-            if not any(count >= 2 for count in occurences.values()):
-                return j
-            break
+    for i in range(4, len(s) + 1):
+        occurences = set(s[i - 4:i])
+        if len(occurences) == 4:
+            return i
 
     return -1
 
 
 def part_2(s: str) -> int:
     """Solve part 2 for day 6 2022."""
-    for i in range(len(s)):
-        for j in range(i + 14, len(s)):
-            occurences = Counter(s[i:j])
-            if not any(count >= 2 for count in occurences.values()):
-                return j
-            break
-
+    for i in range(14, len(s) + 1):
+        occurences = set(s[i - 14:i])
+        if len(occurences) == 14:
+            return i
     return -1
 
 
